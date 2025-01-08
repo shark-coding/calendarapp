f94d8740bcd170a7df2f2b9750ae0aade7f5c020
sanga <tkddk9141@naver.com>
2025-01-08 17:32:19 +0900

    feat: opencsv기능 추가

--- src/main/java/com/project/calendarapp/CalendarappApplication.java
+++ src/main/java/com/project/calendarapp/CalendarappApplication.java
@@ -1,9 +1,11 @@
 package com.project.calendarapp;
 
 import com.project.calendarapp.event.*;
+import com.project.calendarapp.reader.EventCsvReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -14,33 +16,21 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class CalendarappApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
 
-        // 참석자
-        HashSet<String> participants = new HashSet<String>();
-        participants.add("victoria");
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        Meeting meeting1 = new Meeting(
-                1, "meeting1",
-                ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
-                participants, "meetingRoomA",
-                "study"
-        );
-        schedule.add(meeting1);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
-        Todo todo1 = new Todo(
-                2, "todo1",
-                ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4),
-                "할 일 적기"
-        );
-        schedule.add(todo1);
 
         // 전체출력
         schedule.printAll();
 
         // 일부 출력
-        schedule.printBy(EventType.TO_DO);
+//        schedule.printBy(EventType.TO_DO);
     }
 
 }
